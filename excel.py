77c0b2b36dc3c3192522c4deadf7b36efb6980b4
Vish76 <victor.marchais@eleves.enpc.fr>
2016-11-27 21:29:46 +0000

    gestion du doc excel

--- excel.py
+++ excel.py
@@ -1,102 +1,88 @@
 #!/usr/bin/python
 #encoding: utf8
-'''
-Document en cours de création
-
-Attention d'après le système de couche (interface, graphique etc) ce Document
-n'a pas le droit d'importer autre chose (tourelle, affiche, armée... )
-
-Important :
-Fonction Utiles pour les autres docs :
-(aucune pour 'linstant')
 
 '''
-import xlrd
+Atttention
+Executer ce fichier créera un nouveau Excel data vide de tout ce qui a été remplit
+sauf si vous enlever la ligne 61 ou ---book.save("data.xls")---
+'''
+
 from xlwt import Workbook
+
+
+import xlrd
 # ouverture du fichier Excel
-wb = xlrd.open_workbook('test.xls')
-
-# feuilles dans le classeur
-print (wb.sheet_names())
-[u'Feuille1', u'Feuille2', u'Feuille3']
-
-# lecture des données dans la première feuille
-sh = wb.sheet_by_name(u'Feuille1')
-for rownum in range(sh.nrows):
-    print(sh.row_values(rownum))
-[u'id', u'x', u'y', u'test']
-[1.0, 235.0, 424.0, u'a']
-[2.0, 245.0, 444.0, u'b']
-[3.0, 255.0, 464.0, u'c']
-[4.0, 265.0, 484.0, u'd']
-[5.0, 275.0, 504.0, u'e']
-[6.0, 285.0, 524.0, u'f']
-[7.0, 295.0, 544.0, u'g']
-
-# lecture par colonne
-colonne1 = sh.col_values(0)
-print (colonne1)
-[u'id', 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
-
-colonne2=sh.col_values(1)
-print (colonne2)
-[u'x', 235.0, 245.0, 255.0, 265.0, 275.0, 285.0, 295.0]
-
-################################################################
-
-# ouverture du fichier Excel
-wb = xlrd.open_workbook('test.xls')
-
-# feuilles dans le classeurFeuille
-print (wb.sheet_names())
-[u'Feuille1', u'Feuille2', u'Feuille3']
-
-# lecture des données dans la première feuille
-sh = wb.sheet_by_name(u'Feuille1')
-for rownum in range(sh.nrows):
-    print (sh.row_values(rownum))
-[u'id', u'x', u'y', u'test']
-[1.0, 235.0, 424.0, u'a']
-[2.0, 245.0, 444.0, u'b']
-[3.0, 255.0, 464.0, u'c']
-[4.0, 265.0, 484.0, u'd']
-[5.0, 275.0, 504.0, u'e']
-[6.0, 285.0, 524.0, u'f']
-[7.0, 295.0, 544.0, u'g']
-
-# lecture par colonne
-colonne1 = sh.col_values(0)
-print(colonne1)
-[u'id', 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
-
-colonne2=sh.col_values(1)
-print(colonne2)
-[u'x', 235.0, 245.0, 255.0, 265.0, 275.0, 285.0, 295.0]
-
-# extraction d'un élément par
-
-
+wb = xlrd.open_workbook('data.xls')
 # création
 book = Workbook()
 
 # création de la feuille 1
-feuil1 = book.add_sheet('feuille 1')
 
-# ajout des en-têtes
+# Ennemis
+feuil1 = book.add_sheet('armee')
 feuil1.write(0,0,'id')
-feuil1.write(0,1,'x')
-feuil1.write(0,2,'y')
-feuil1.write(0,3,'test')
+feuil1.write(0,1,'nom')
+feuil1.write(0,2,'pv')
+feuil1.write(0,3,'defense')
+feuil1.write(0,4,'vitesse')
+feuil1.write(0,5,'attaque')
 
 # ajout des valeurs dans la ligne suivante
 ligne1 = feuil1.row(1)
+ligne1.write(1,'Bowser')
 ligne1.write(0,'1')
-ligne1.write(1,'235.0')
-ligne1.write(2,'424.0')
-ligne1.write(3,'a')
-
+ligne1.write(2,'100')
+ligne1.write(3,'1')
+ligne1.write(4,'1')
+ligne1.write(5,'1')
+ligne1.write(6,'bowser.jpg')
 # ajustement éventuel de la largeur d'une colonne
 feuil1.col(0).width = 10000
 
 # éventuellement ajout d'une autre feuille 2
-feuil2 = book.add_sheet('feuille 2')
+feuil2 = book.add_sheet('tourelle')
+feuil2.write(0,0,'id')
+feuil2.write(0,1,'nom')
+feuil2.write(0,2,'pv')
+feuil2.write(0,3,'defense')
+feuil2.write(0,4,'vitesse')
+#projectile donne l'ID du projectile
+feuil2.write(0,5,'projectile')
+
+feuil3 = book.add_sheet('projectile')
+feuil3.write(0,0,'id')
+feuil3.write(0,1,'nom')
+#si le projectile est un gaz ou un cercle de feu
+feuil3.write(0,2,'rayon')
+feuil3.write(0,3,'vitesse')
+feuil3.write(0,4,'vitesse')
+#projectile donne l'ID du projectile
+
+book.save("data.xls")
+'''
+Lourd, il faut ouvrir tout le temps le doc c'est peut-être lent
+data_python peut être global
+'''
+
+def extract(feuille,id_data,donnee):
+#id correspond à la ligne
+#donne est la colonne (entier)
+    wb = xlrd.open_workbook('data.xls')
+    sh=wb.sheet_by_name(feuille)
+    return sh.col_values(donnee)[id_data]
+
+def extract2(feuille,id_data,donnee):
+    wb=xlrd.open_workbook('data.xls')
+    sh=wb.sheet_by_name(feuille)
+    x=1
+    while(x<10):
+        if(donnee==sh.col_values(x)[0]):
+            return sh.col_values(x)[id_data]
+        else:
+            x=x+1
+    assert(False)
+    #vous avez demander une donnée qui n'est pas répertoriée
+
+print(extract2("armee",1,"pv"))
+# création matérielle du fichier résultant
+book.save('data.xls')
